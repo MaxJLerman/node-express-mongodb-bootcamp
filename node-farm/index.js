9e8ade1a8383876172b388196a7584a27610dacb
Max Lerman <mjl82601@gmail.com>
2024-05-04 20:17:54 +0100

    finished code

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 const replaceTemplate = (template, product) => {
   let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
@@ -18,7 +19,7 @@ const replaceTemplate = (template, product) => {
   return output;
 };
 
-//! 3 synchronous functions below cannot be called insude createServer callback function
+//! 3 synchronous functions below CANNOT be called insude createServer callback function
 const templateOverview = fs.readFileSync(
   `${__dirname}/templates/overview.html`,
   "utf-8",
@@ -36,7 +37,7 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((request, response) => {
-  const pathname = request.url;
+  const { query, pathname } = url.parse(request.url, true);
 
   if (pathname === "/" || pathname === "/overview") {
     response.writeHead(200, {
@@ -46,21 +47,30 @@ const server = http.createServer((request, response) => {
     const cardsHTML = dataObj
       .map((element) => replaceTemplate(templateCard, element))
       .join("");
-
     const output = templateOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHTML);
+
     response.end(output);
-  } else if (pathname === "/products") {
-    response.end("This is the products");
+  } else if (pathname === "/product") {
+    response.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+
+    const product = dataObj[query.id];
+    const output = replaceTemplate(templateProduct, product);
+
+    response.end(output);
   } else if (pathname === "/api") {
     response.writeHead(200, {
       "Content-Type": "application/json",
     });
+
     response.end(data);
   } else {
     response.writeHead(404, {
       "Content-Type": "text/html",
       "my-own-header": "hello world",
     });
+
     response.end("<h1>Page not found :(</h1>");
   }
 });
