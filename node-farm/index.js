614481cd8b165aa3f75e2bc65bca6b3763183462
Max Lerman <mjl82601@gmail.com>
2024-05-04 20:23:15 +0100

    extracted function into separate module

--- node-farm/index.js
+++ node-farm/index.js
@@ -2,22 +2,7 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
-const replaceTemplate = (template, product) => {
-  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  }
-
-  return output;
-};
+const replaceTemplate = require("./modules/replaceTemplate");
 
 //! 3 synchronous functions below CANNOT be called insude createServer callback function
 const templateOverview = fs.readFileSync(
