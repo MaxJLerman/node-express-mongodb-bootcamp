0e072c7f86b37aed4bbe4caf75000df1e6c8f4a0
Max Lerman <mjl82601@gmail.com>
2024-05-16 13:48:52 +0100

    feat: handling GET requests

--- natours/app.js
+++ natours/app.js
@@ -1,14 +1,29 @@
+const fs = require("fs");
 const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello from server", app: "Natours" });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
+);
+
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
-app.post("/", (req, res) => {
-  res.send("You can post to this endpoint");
-});
+// app.get("/", (req, res) => {
+//   res.status(200).json({ message: "Hello from server", app: "Natours" });
+// });
+
+// app.post("/", (req, res) => {
+//   res.send("You can post to this endpoint");
+// });
 
 const port = 3000;
 app.listen(port, () => {
