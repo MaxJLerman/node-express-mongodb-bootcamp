030c46d868cce9c5c445910e72f706bf87f6b366
Max Lerman <mjl82601@gmail.com>
2024-05-28 15:12:36 +0100

    feat: created tour schema & model

--- natours/server.js
+++ natours/server.js
@@ -12,6 +12,23 @@ mongoose.connect(database).then(() => {
   console.log("database connection successful");
 });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A tour must have a name"],
+    unique: true,
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour must have a price"],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+});
+const Tour = mongoose.model("Tour", tourSchema);
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on port ${port}...`);
