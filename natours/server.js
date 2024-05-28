ad078ab9e085ba85bb97860a0e7ec20f16d5c3f4
Max Lerman <mjl82601@gmail.com>
2024-05-28 15:22:05 +0100

    feat: creating documents & testing the model

--- natours/server.js
+++ natours/server.js
@@ -29,6 +29,20 @@ const tourSchema = new mongoose.Schema({
 });
 const Tour = mongoose.model("Tour", tourSchema);
 
+const testTour = new Tour({
+  name: "The Park Camper",
+  price: 997,
+});
+
+testTour
+  .save()
+  .then((document) => {
+    console.log(document);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on port ${port}...`);
