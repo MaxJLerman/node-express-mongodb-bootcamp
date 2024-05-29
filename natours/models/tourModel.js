867562a2939eeca1b51c168b00dcee46ec968168
Max Lerman <mjl82601@gmail.com>
2024-05-29 17:02:09 +0100

    feat: implemented document middleware

--- natours/models/tourModel.js
+++ natours/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, "A tour must have a duration"],
@@ -62,6 +64,26 @@ const tourSchema = new mongoose.Schema(
 
 tourSchema.virtual("durationWeeks").get(function () {
   return this.duration / 7;
+});
+
+//* pre-save hook: middleware that runs before a "save" or "create" but not "insertMany" event (document is saved to database)
+tourSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+
+  next();
+});
+
+tourSchema.pre("save", function (next) {
+  console.log("Saving...");
+
+  next();
+});
+
+//* post-save hook: middleware that runs after a "save" or "create" but not "insertMany" event
+tourSchema.post("save", function (document, next) {
+  console.log(document);
+
+  next();
 });
 
 const Tour = mongoose.model("Tour", tourSchema);
