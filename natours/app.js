a5f80ccf9c29a91925ba15a13436d41808b20f20
Max Lerman <mjl82601@gmail.com>
2024-05-30 15:16:01 +0100

    feat: implementing a global error handling middleware

--- natours/app.js
+++ natours/app.js
@@ -23,9 +23,25 @@ app.use("/api/v1/tours", tourRouter); //* using middleware function created in a
 app.use("/api/v1/users", userRouter);
 
 app.all("*", (request, response, next) => {
-  response.status(404).json({
-    status: "fail",
-    message: `Can't find ${request.originalUrl} on the server`,
+  // response.status(404).json({
+  //   status: "fail",
+  //   message: `Can't find ${request.originalUrl} on the server`,
+  // });
+
+  const err = new Error(`Can't find ${request.originalUrl} on the server`);
+  err.status = "fail";
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((error, request, response, next) => {
+  error.statusCode = error.statusCode || 500;
+  error.status = error.status || "error";
+
+  response.status(error.statusCode).json({
+    status: error.status,
+    message: error.message,
   });
 });
 
