ab4e4f0715f378bad625a9ea844cda6674b1b9d4
Max Lerman <mjl82601@gmail.com>
2024-05-30 15:06:53 +0100

    feat: handling unhandled routes

--- natours/app.js
+++ natours/app.js
@@ -12,19 +12,21 @@ if (process.env.NODE_ENV === "development") {
 app.use(express.json()); //* middleware, in the middle of the request & response, gives us access to props on req parameter
 app.use(express.static(`${__dirname}/public`));
 
-//* custom muddlewares
-app.use((req, res, next) => {
-  console.log("hello from the middleware");
-  next();
-});
+app.use((request, response, next) => {
+  request.requestTime = new Date().toISOString();
 
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
   next();
 });
 
 app.use("/api/v1/tours", tourRouter); //* using middleware function created in another file
 //* now created a sub application (router system) for "tours" resource
 app.use("/api/v1/users", userRouter);
+
+app.all("*", (request, response, next) => {
+  response.status(404).json({
+    status: "fail",
+    message: `Can't find ${request.originalUrl} on the server`,
+  });
+});
 
 module.exports = app;
