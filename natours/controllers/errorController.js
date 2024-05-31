fe1f251e0f950c93980d9303c4ee19f32551f541
Max Lerman <mjl82601@gmail.com>
2024-05-31 15:20:56 +0100

    feat: implemented errors during development vs production

--- natours/controllers/errorController.js
+++ natours/controllers/errorController.js
@@ -1,9 +1,38 @@
+const sendErrorDevelopment = (error, response) => {
+  response.status(error.statusCode).json({
+    error,
+    status: error.status,
+    message: error.message,
+    stack: error.stack,
+  });
+};
+
+const sendErrorProduction = (error, response) => {
+  //? operational, trusted error: send message to client
+  if (error.isOperational) {
+    response.status(error.statusCode).json({
+      message: error.message,
+      stack: error.stack,
+    });
+  }
+  //? programming or other unknown error: don't leak error details
+  else {
+    console.error("ERROR", error);
+
+    response.status(500).json({
+      status: "Error",
+      message: "Something went wrong",
+    });
+  }
+};
+
 module.exports = (error, request, response, next) => {
   error.statusCode = error.statusCode || 500;
   error.status = error.status || "error";
 
-  response.status(error.statusCode).json({
-    status: error.status,
-    message: error.message,
-  });
+  if (provess.env.NODE_ENV === "development") {
+    sendErrorDevelopment(error, response);
+  } else if (provess.env.NODE_ENV === "production") {
+    sendErrorProduction(error, response);
+  }
 };
