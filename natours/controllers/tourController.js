afae1d74d479a4bdaad1b75d3fb32cc3e49c3a8e
Max Lerman <mjl82601@gmail.com>
2024-05-31 15:10:20 +0100

    feat: implemented 404 not found errors

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -1,6 +1,7 @@
 const Tour = require("../models/tourModel");
 const APIFeatures = require("../utils/apiFeatures");
 const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
 
 exports.aliasTopTours = async (request, response, next) => {
   request.query.limit = "5";
@@ -11,13 +12,12 @@ exports.aliasTopTours = async (request, response, next) => {
 };
 
 exports.getAllTours = catchAsync(async (request, response, next) => {
-  //* build query
   const features = new APIFeatures(Tour.find(), request.query)
     .filter()
     .sort()
     .limitFields()
     .paginate();
-  const tours = await features.query; //* execute built query
+  const tours = await features.query;
 
   response.status(200).json({
     status: "success",
@@ -31,6 +31,10 @@ exports.getAllTours = catchAsync(async (request, response, next) => {
 
 exports.getOneTour = catchAsync(async (request, response, next) => {
   const tour = await Tour.findById(request.params.id);
+
+  if (!tour) {
+    return next(new AppError("No tour found with that ID", 404));
+  }
 
   response.status(200).json({
     status: "success",
@@ -57,6 +61,10 @@ exports.updateTour = catchAsync(async (request, response, next) => {
     runValidators: true,
   });
 
+  if (!tour) {
+    return next(new AppError("No tour found with that ID", 404));
+  }
+
   response.status(200).json({
     status: "success",
     data: {
@@ -66,7 +74,11 @@ exports.updateTour = catchAsync(async (request, response, next) => {
 });
 
 exports.deleteTour = catchAsync(async (request, response, next) => {
-  await Tour.findByIdAndDelete(request.params.id);
+  const tour = await Tour.findByIdAndDelete(request.params.id);
+
+  if (!tour) {
+    return next(new AppError("No tour found with that ID", 404));
+  }
 
   response.status(204).json({
     status: "success",
