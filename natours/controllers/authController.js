93a98970a7d0354ffbe108efa240fe673edaff6e
Max Lerman <mjl82601@gmail.com>
2024-06-04 15:11:43 +0100

    feat: implemented signing up users

--- natours/controllers/authController.js
+++ natours/controllers/authController.js
@@ -1,11 +1,24 @@
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
 
 exports.signup = catchAsync(async (request, response, next) => {
-  const newUser = await User.create(request.body);
+  const { name, email, password, confirmPassword } = request.body;
+
+  const newUser = await User.create({
+    name,
+    email,
+    password,
+    confirmPassword,
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   response.status(201).json({
     status: "success",
+    token,
     data: {
       user: newUser,
     },
